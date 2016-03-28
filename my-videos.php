eef32dce4cf5dd142782a15b8afa2bd1acf8b106
srikar.ch <srikar.0896@gmail.com>
2016-03-28 15:26:03 +0530

    Enhancement
    Added uploading option and editing privacy of the media directly during upload rather than editing in the media_mangement

--- my-videos.php
+++ my-videos.php
@@ -1,11 +1,6 @@
 <!DOCTYPE html>
 <html class="html" lang="en-US">
  <head>
-
-  <script type="text/javascript">
-   if(typeof Muse == "undefined") window.Muse = {}; window.Muse.assets = {"required":["jquery-1.8.3.min.js", "museutils.js", "jquery.scrolleffects.js", "jquery.watch.js", "jquery.musemenu.js", "my-media.css"], "outOfDate":[]};
-</script>
-  
   <meta http-equiv="Content-type" content="text/html;charset=UTF-8"/>
   <meta name="generator" content="2015.0.2.310"/>
   <title>My media</title>
@@ -14,7 +9,7 @@
   <link rel="stylesheet" type="text/css" href="css/my-media.css?3960586673" id="pagesheet"/>
   
   <!-- Other scripts -->
-  
+   <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
   <!-- JS includes -->
   <!--[if lt IE 9]>
   <script src="scripts/html5shiv.js?4241844378" type="text/javascript"></script>
@@ -50,10 +45,110 @@
      </nav>
     </div>
     <img class="colelem" id="u153-4" alt="your uploads" width="410" height="100" src="images/u153-4.png"/><!-- rasterized frame -->
-    <div class="colelem" id="u431"><!-- custom html -->
+    
+	<div style="">
+	
+	</div>
+	<div class="colelem" id="u431"><!-- custom html -->
      <html>
 
 <head>
+<link rel='stylesheet' href='css/my-media-data.css'>
+</head>
+<body>
+<center>
+<div style="border:1px solid #1565C0;width:20%;border-radius:5px;float:right;margin-top:-100px;margin-left:40px;padding:10px;" >
+<p>VISIT</p>
+<center>
+<a href="my-videos.php">your videos</a><br>
+<a href="my-media.php">your music</a><br>
+<a href="youtube_playlist.php">youtube playlist</a><br>
+<a href="soundcloud_playlist.php">soundcloud playlist</a><br>
+
+</center>
+</div>
+</center>
+</body>
+
+</html>
+
+<form action="<?php $current_page?>" method="POST" enctype="multipart/form-data">
+    Upload Music here:
+    <input type="text" name="name" value="Enter track Name">
+    <br><br>Choose privacy :&nbsp
+	<label>
+        <select name="view">
+		<option value="1">Public</option>
+		<option value="0">Private</option>
+		</select>
+		</label>
+	<br><br>
+    <input type="file" name="file" >
+    <input type="submit" value="Upload Video" name="submit">
+</form>
+<br><br>
+<html>
+
+<head>
+<style>
+p,a{
+	font-family: 'Oswald', sans-serif;
+	font-style:bold;
+	font-size:14px;
+	color:#212121;
+	
+}
+
+select {
+    padding:3px;
+    margin: 0;
+	width:100px;
+    -webkit-border-radius:4px;
+    -moz-border-radius:4px;
+    border-radius:4px;
+    -webkit-box-shadow: 0 3px 0 #ccc, 0 -1px #fff inset;
+    -moz-box-shadow: 0 3px 0 #ccc, 0 -1px #fff inset;
+    box-shadow: 0 3px 0 #ccc, 0 -1px #fff inset;
+    background: #f8f8f8;
+    color:#212121;
+    border-bottom:none;
+    outline:none;
+    display: inline-block;
+    -webkit-appearance:none;
+    -moz-appearance:none;
+    appearance:none;
+    cursor:pointer;
+}
+@media screen and (-webkit-min-device-pixel-ratio:0) {
+    select {padding-right:18px}
+}
+
+label {position:relative}
+label:after {
+    content:'<>';
+    font:11px "Consolas", monospace;
+    color:#aaa;
+    -webkit-transform:rotate(90deg);
+    -moz-transform:rotate(90deg);
+    -ms-transform:rotate(90deg);
+    transform:rotate(90deg);
+    right:8px; top:2px;
+    padding:0 0 2px;
+    border-bottom:1px solid #ddd;
+    position:absolute;
+    pointer-events:none;
+}
+label:before {
+    content:'';
+    right:6px; top:0px;
+    width:20px; height:20px;
+    background:#f8f8f8;
+    position:absolute;
+    pointer-events:none;
+    display:block;
+}
+
+</style>
 <link rel='stylesheet' href='css/my-media-data.css'>
 </head>
 <body>
@@ -63,17 +158,6 @@
 
 
 
-<html>
-
-<head>
-<link rel='stylesheet' href='style.css'>
-</head>
-<body>
-</body>
-
-</html>
-
-
 
 <?php
 error_reporting(0);
@@ -81,52 +165,113 @@ error_reporting(0);
 //require 'connect.inc.php';
 //$limit=1;
 //$query='SELECT `data`,`data_link` FROM `records` WHERE `id` LIMIT {'$limit'}';
-echo '<center>';
-echo $show=$_SESSION['user_name'].' DATABASE';
-echo '</center>';
-echo '<br><br>';
+//echo $show=$_SESSION['user_name'].' DATABASE';
 $mysql_host='localhost';
 $mysql_user='root';
 $mysql_password='';
+if(isset($_SESSION['user_name']) && !empty($_SESSION['user_name'])){
 $mysql_db=$_SESSION['user_name'];
 if(@(mysql_connect($mysql_host,$mysql_user,$mysql_password))&&(@(mysql_select_db($mysql_db)))){
+	
+	
+	if(isset($_POST['name'])){$target_dir = "uploads/";
+echo '<br>';
+$target_file = $target_dir . $_POST['name'].".mp3";
 
-$query="SELECT `data`,`data_link` FROM `videos` ORDER By `id` DESC LIMIT 0,3";
+echo '<br>';
+//$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+//echo 'started';
+echo '<br>';    
+$check = $_FILES['file']['tmp_name'];
+    if($check !== false) {
+        move_uploaded_file($check,$target_file);
+		echo 'Uploaded Succesfully to your database';
+		 } else {
+        echo "Please select valid .mp3 file.";
+            }
+}
+
+	
+	$uploading_query="INSERT INTO ".$mysql_db.".`music` (`id`, `data`, `data_link`, `view`) VALUES (NULL, '".$_POST['name']."', '".$_POST['name'].".mp3', '".$_POST['view']."');";
+    mysql_query($uploading_query);}
+	}
+
+
+	
+
+$query="SELECT `data`,`data_link` FROM `music` WHERE `view`='1' ORDER By `id` DESC";
 
 $query_run=mysql_query($query);
 while($result_array=mysql_fetch_array($query_run,MYSQL_ASSOC)){
-   $channel = array('title' => "What's For Dinner",
+   $channel = array(
       'description' =>$result_array['data']);
    
    echo "<channel>\n";
-   echo '<center>';
+   
    foreach ($channel as $element => $content) {
-	   $url="videos/".$result_array['data_link'];
       echo " <$element>";
       echo htmlentities($content);
       echo "</$element>\n";
-	
+	  
+	  
    }
+ $url="uploads/".$result_array['data_link'];
+echo '<audio controls>';
+echo "<source src='$url' type='audio/mpeg'>";
+echo '</audio>';
+echo "</channel>";
    
-   echo '<video width="400" controls>';
-    echo "<source src='$url' type='video/mp4'>";
-    echo '</video>';
-    echo '</center>';
-	echo "</channel>";
-	echo '<br><br>';
-   
- }
- echo "<br><br>";
-echo '<center><b><a href="logout.php">LOGOUT</a></b></center>'; 
-
-}else{
-	echo '<br>';
-	echo 'Welocme anonymous! Permission denied.';
-	echo '<br><br><b><a href="logout.php"></a></b>';
 }
+/*personal media*/
+echo '<hr>';
+echo '<center>';
+echo '<p style="font-size:30px">PERSONAL MEDIA</p>';
+echo '</center>';
+$query1="SELECT `data`,`data_link` FROM `music` WHERE `view`='0' ORDER By `id` DESC";
+
+$query_run1=mysql_query($query1);
+while($result_array=mysql_fetch_array($query_run1,MYSQL_ASSOC)){
+   $channel = array(
+      'description' =>$result_array['data']);
+   
+   echo "<channel>\n";
+   
+   foreach ($channel as $element => $content) {
+      echo " <$element>";
+      echo htmlentities($content);
+      echo "</$element>\n";
+	  
+	  
+   }
+ $url="uploads/".$result_array['data_link'];
+echo '<audio controls>';
+echo "<source src='$url' type='audio/mpeg'>";
+echo '</audio>';
+echo "</channel>";
+
+
+   }
+ echo '<br><br>';
+ echo  '<center><b><a href="logout.php">LOGOUT</a></b></center>'; 
+
+}
+	else {
+	 echo 'WELCOME ANONYMOUS!<br>ACCESS DENIED!!';
+	 echo '<br><br>';
+	 echo '<center><b><a href="logout.php">LOGIN</a></b></center>';
+    }
+/*else{
+	echo '<br>';
+	echo 'OOPS!Please login to view your uploads.';
+	echo '<br><br><center><b><a id="logout" href="index.php">LOGIN</a></b></center>';
+    
+	}*/
 ?>
 
 <!--<input type="button" name="logout" value="logout" onclick='logout.php'/> -->
+
 
 
 </div>
@@ -156,7 +301,8 @@ echo '<center><b><a href="logout.php">LOGOUT</a></b></center>';
    $(document).ready(function() { try {
 (function(){var a={},b=function(a){if(a.match(/^rgb/))return a=a.replace(/\s+/g,"").match(/([\d\,]+)/gi)[0].split(","),(parseInt(a[0])<<16)+(parseInt(a[1])<<8)+parseInt(a[2]);if(a.match(/^\#/))return parseInt(a.substr(1),16);return 0};(function(){$('link[type="text/css"]').each(function(){var b=($(this).attr("href")||"").match(/\/?css\/([\w\-]+\.css)\?(\d+)/);b&&b[1]&&b[2]&&(a[b[1]]=b[2])})})();(function(){$("body").append('<div class="version" style="display:none; width:1px; height:1px;"></div>');
 for(var c=$(".version"),d=0;d<Muse.assets.required.length;){var f=Muse.assets.required[d],g=f.match(/([\w\-\.]+)\.(\w+)$/),k=g&&g[1]?g[1]:null,g=g&&g[2]?g[2]:null;switch(g.toLowerCase()){case "css":k=k.replace(/\W/gi,"_").replace(/^([^a-z])/gi,"_$1");c.addClass(k);var g=b(c.css("color")),h=b(c.css("background-color"));g!=0||h!=0?(Muse.assets.required.splice(d,1),"undefined"!=typeof a[f]&&(g!=a[f]>>>24||h!=(a[f]&16777215))&&Muse.assets.outOfDate.push(f)):d++;c.removeClass(k);break;case "js":k.match(/^jquery-[\d\.]+/gi)&&
-typeof $!="undefined"?Muse.assets.required.splice(d,1):d++;break;default:throw Error("Unsupported file type: "+g);}}c.remove();if(Muse.assets.outOfDate.length||Muse.assets.required.length)c="Some files on the server may be missing or incorrect. Clear browser cache and try again. If the problem persists please contact website author.",(d=location&&location.search&&location.search.match&&location.search.match(/muse_debug/gi))&&Muse.assets.outOfDate.length&&(c+="\nOut of date: "+Muse.assets.outOfDate.join(",")),d&&Muse.assets.required.length&&(c+="\nMissing: "+Muse.assets.required.join(",")),alert(c)})()})();/* body */
+typeof $!="undefined"?Muse.assets.required.splice(d,1):d++;break;default:throw Error("Unsupported file type: "+g);}}c.remove();if(Muse.assets.outOfDate.length||Muse.assets.required.length)c="Some files on the server may be missing or incorrect. Clear browser cache and try again. If the problem persists please contact website author.",(d=location&&location.search&&location.search.match&&location.search.match(/muse_debug/gi))&&Muse.assets.outOfDate.length&&(c+="\nOut of date: "+Muse.assets.outOfDate.join(",")),d&&Muse.assets.required.length&&(c+="\nMissing: "+Muse.assets.required.join(",")),alert(c)})()})();
+/* body */
 Muse.Utils.transformMarkupToFixBrowserProblemsPreInit();/* body */
 Muse.Utils.prepHyperlinks(true);/* body */
 $('#u146').registerBackgroundPositionScrollEffect([{"speed":[0,0.4],"in":[-Infinity,-100]},{"speed":[0,0.4],"in":[-100,Infinity]}]);/* scroll effect */
